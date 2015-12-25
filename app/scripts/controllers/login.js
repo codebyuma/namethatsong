e033777983d94540e01157faba7a6e44aa749161
Uma Chandran <umachandran6@gmail.com>
2015-12-25 07:46:28 -0500

    starting to get user spotifyAuth working

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,6 +1,36 @@
 'use strict';
 angular.module('ntsApp').controller('loginCtrl', function($scope, $rootScope, $http) {
           var stateKey = 'spotify_auth_state';
+          console.log("hash", getHashParams());
+
+          var params = getHashParams();
+              $scope.access_token = params["/access_token"];
+              var state = params.state,
+              storedState = localStorage.getItem(stateKey);
+
+              console.log("params", params)
+              console.log("access token", $scope.access_token)
+              console.log("state", state)
+              console.log("stored state", storedState);
+
+                if ($scope.access_token && (state == null || state !== storedState)) {
+                  alert('There was an error during the authentication');
+                } else {
+                  localStorage.removeItem(stateKey);
+                  if ($scope.access_token) {
+
+                    $scope.getProfile($scope.access_token)
+                    .then (function(response){
+                      console.log("wooo", response);
+                      $scope.loggedIn = true;
+                    })
+
+                  } else {
+                      // $('#login').show();
+                      // $('#loggedin').hide();
+                      $scope.loggedIn = false;
+                  }
+                }
           
           // implicit grant flow
 
@@ -33,30 +63,21 @@ angular.module('ntsApp').controller('loginCtrl', function($scope, $rootScope, $h
 
           // get auth from the user by making get request to spotify.com/authorize
 
-          /*XMLHttpRequest cannot load https://accounts.spotify.com/authorize?response_type=token&client_id=7025ce…user-read-email&redirect_uri=http://localhost:9000/&state=YcVUpXF4n9M4RKeg. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:9000' is therefore not allowed access.*/
 
+          // making an http request to get auth doesn't work - they want us to redirect via the browser
           $scope.requestAuth = function (){
               var state = generateRandomString(16);
               localStorage.setItem(stateKey, state);
 
                 var url = 'https://accounts.spotify.com/authorize'
                     url += '?response_type=token';
-                    url += '&client_id=' + '7025ce5e8b8d4e839abd1fc0ba0fff17';
-                    url += '&scope=' + 'user-read-private user-read-email';
-                    url += '&redirect_uri=' + 'http://localhost:9000/';
-                    url += '&state=' + generateRandomString(16);
+                    url += '&client_id=' + encodeURIComponent('7025ce5e8b8d4e839abd1fc0ba0fff17');
+                    url += '&scope=' + encodeURIComponent('user-read-private user-read-email');
+                    url += '&redirect_uri=' + encodeURIComponent('http://localhost:9000/');
+                    url += '&state=' + encodeURIComponent(state);
 
-
-                return $http.get(url)
-                .then (function(response){
-                  console.log('response: ', response.data);
-                  return response.data;
-                }, function (error){
-                  //console.log('failed auth', error.data.error.message);
-                  console.log('failed auth', error);
-                  return error;
-                });
-
+                    window.location = url;
+                  
 
           }
 
@@ -65,40 +86,12 @@ angular.module('ntsApp').controller('loginCtrl', function($scope, $rootScope, $h
           // call requestAuth, then call getProfile to display it. 
           // will ADD TOKEN to scope for use in rest of app
           $scope.loginUser = function (){
-            
             $scope.requestAuth()
-            .then(function (response){
-
-              var params = getHashParams();
-              $scope.access_token = params.access_token;
-              var state = params.state,
-              storedState = localStorage.getItem(stateKey);
-
-                if ($scope.access_token && (state == null || state !== storedState)) {
-                  alert('There was an error during the authentication');
-                } else {
-                  localStorage.removeItem(stateKey);
-                  if ($scope.access_token) {
-
-                    $scope.getProfile($scope.access_token)
-                    .then (function(response){
-                      console.log("wooo", response);
-                      $scope.loggedIn = true;
-                    })
-
-                  } else {
-                      // $('#login').show();
-                      // $('#loggedin').hide();
-                      $scope.loggedIn = false;
-                  }
-                }
-
-            }, function (error){
-              console.log("error", error);
-            });
-            
-
           }
+
+          
+             
+          
 
           // once user has authorized and we have their token, use it to get profile info
           // make get request to spotify/me
